3eaa318b1ebf8bd6cff911d3480558127723d2e6
mendo94 <rinamendozaa@gmail.com>
2022-07-02 22:55:16 -0400

    moved jsx code to expenses and object to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem"; //look for expense component in component folder
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses"; //look for expense component in component folder
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2022, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2022, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2022, 5, 12),
+    },
+  ];
   return (
     <div>
-      <Expenses />
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
