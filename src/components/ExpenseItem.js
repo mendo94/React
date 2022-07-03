dc8bc625c3ec372e2ef4d57eed698cd63e939ea6
mendo94 <rinamendozaa@gmail.com>
2022-07-02 21:01:26 -0400

    optimize placeholders

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,21 @@
-function ExpenseItem() {
-  return <h2>Expense item!</h2>;
-}
+import "./ExpenseItem.scss";
+
+const ExpenseItem = () => {
+  const expenseDate = new Date(2022, 7, 2).toDateString;
+
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 238.97;
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
+  );
+};
 // custom component
+//date object has to be outputted as a string
 
 export default ExpenseItem;
