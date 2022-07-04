7a8a8c643dd58f326ffa85ec816178c0f4f224cc
mendo94 <rinamendozaa@gmail.com>
2022-07-04 18:05:05 -0400

    removed wrong default bug

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.scss";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title); //react state hook for data that may change needs state to change values
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
